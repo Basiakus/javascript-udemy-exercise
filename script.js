038f69fdde407585ccfdd61d0462eaafb3dba06a
Adam Basiak <basiakus@gmail.com>
2018-04-12 21:41:59 +0200

    add window.onload func

--- script.js
+++ script.js
@@ -1,58 +1,60 @@
-const test = document.getElementById('test');
-const elementThis = document.getElementById('elementThis'); 
-const textSize = document.getElementById('textSize');
-const textSizeButtons = document.getElementById('textSizeButtons');
-const pxDisplay = document.createElement('input');
-const buttonPlus = document.createElement('button');
-const buttonMinus = document.createElement('button');
-let fontSize = 14;
+window.onload = function() {
 
-textSizeButtons.appendChild(pxDisplay);
-textSizeButtons.appendChild(buttonPlus);
-textSizeButtons.appendChild(buttonMinus);
+	const test = document.getElementById('test');
+	const elementThis = document.getElementById('elementThis'); 
+	const textSize = document.getElementById('textSize');
+	const textSizeButtons = document.getElementById('textSizeButtons');
+	const pxDisplay = document.createElement('input');
+	const buttonPlus = document.createElement('button');
+	const buttonMinus = document.createElement('button');
+	let fontSize = 14;
+
+	textSizeButtons.appendChild(pxDisplay);
+	textSizeButtons.appendChild(buttonPlus);
+	textSizeButtons.appendChild(buttonMinus);
 
 
-buttonPlus.innerHTML = "+";
-buttonMinus.innerHTML = "-";
+	buttonPlus.innerHTML = "+";
+	buttonMinus.innerHTML = "-";
 
-function answer() {
-	alert('Ejjj!!!!')
-}
-
-function changeColor() {
-	if (this.style.color == 'black') {
-		this.style.color = 'red';
-	} else {
-		this.style.color = 'black';
+	function answer() {
+		alert('Ejjj!!!!')
 	}
-}
 
-function fontSizePlus() {
-	if (fontSize < 32) {
-		fontSize += 1;
-		fontSize.toString();
-		pxDisplay.value = fontSize + 'px';
-		textSize.style.fontSize = fontSize + 'px';
-	} else {
-		return;
+	function changeColor() {
+		if (this.style.color == 'black') {
+			this.style.color = 'red';
+		} else {
+			this.style.color = 'black';
+		}
 	}
-}
 
-function fontSizeMinus() {
-		if (fontSize > 0) {
-		fontSize -= 1;
-		fontSize.toString();
-		pxDisplay.value = fontSize + 'px';
-		textSize.style.fontSize = fontSize + 'px';
-	} else {
-		return;
-	} 
-}
+	function fontSizePlus() {
+		if (fontSize < 32) {
+			fontSize += 1;
+			pxDisplay.value = fontSize + 'px';
+			textSize.style.fontSize = fontSize + 'px';
+		} else {
+			return;
+		}
+	}
 
-elementThis.onclick = answer;
-elementThis.onmouseover = changeColor;
-buttonPlus.addEventListener('click', fontSizePlus);
-buttonMinus.addEventListener('click', fontSizeMinus);
+	function fontSizeMinus() {
+			if (fontSize > 0) {
+			fontSize -= 1;
+			pxDisplay.value = fontSize + 'px';
+			textSize.style.fontSize = fontSize + 'px';
+		} else {
+			return;
+		} 
+	}
+
+	elementThis.onclick = answer;
+	elementThis.onmouseover = changeColor;
+	buttonPlus.addEventListener('click', fontSizePlus);
+	buttonMinus.addEventListener('click', fontSizeMinus);
+
+}
 
 
 
