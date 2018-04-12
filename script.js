bd197ce9c9ccc4de510d34a1644a36fdd973a9df
Adam Basiak <basiakus@gmail.com>
2018-04-13 00:04:47 +0200

    next exercise

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 window.onload = function() {
 
+
+	//zadanie 1
 	const test = document.getElementById('test');
 	const elementThis = document.getElementById('elementThis'); 
 	const textSize = document.getElementById('textSize');
@@ -13,14 +15,12 @@ window.onload = function() {
 	textSizeButtons.appendChild(buttonPlus);
 	textSizeButtons.appendChild(buttonMinus);
 
-
 	buttonPlus.innerHTML = "+";
 	buttonMinus.innerHTML = "-";
 
 	function answer() {
 		alert('Ejjj!!!!')
 	}
-
 	function changeColor() {
 		if (this.style.color == 'black') {
 			this.style.color = 'red';
@@ -28,7 +28,6 @@ window.onload = function() {
 			this.style.color = 'black';
 		}
 	}
-
 	function fontSizePlus() {
 		if (fontSize < 32) {
 			fontSize += 1;
@@ -38,7 +37,6 @@ window.onload = function() {
 			return;
 		}
 	}
-
 	function fontSizeMinus() {
 			if (fontSize > 0) {
 			fontSize -= 1;
@@ -48,12 +46,37 @@ window.onload = function() {
 			return;
 		} 
 	}
-
 	elementThis.onclick = answer;
 	elementThis.onmouseover = changeColor;
 	buttonPlus.addEventListener('click', fontSizePlus);
 	buttonMinus.addEventListener('click', fontSizeMinus);
 
+	//zadanie 2
+	const clientXid = document.getElementById('clientXid');
+	const shiftKeyId = document.getElementById('shiftKeyId');
+	const mouseButtonsId = document.getElementById('mouseButtonsId');
+
+	function eventClientX(event) {
+		eventDisplay.value = event.clientX;
+	}
+	function eventShiftKey(event) {
+		eventDisplay.value = event.shiftKey;
+	}
+	function eventButtonMouse(event) {
+
+		eventDisplay.value = event.button;
+		if(event.button === 0) {
+			eventDisplay.value = 'Left mouse button';
+		} else if (event.button === 2) {
+			eventDisplay.value = 'Right mouse button';
+		} else {
+			eventDisplay.value = 'Middle mouse button';
+		}
+	}
+
+	clientXid.addEventListener('mousemove', eventClientX);
+	shiftKeyId.addEventListener('mousemove', eventShiftKey);
+	mouseButtonsId.addEventListener('mousedown', eventButtonMouse);
 }
 
 
