4670b688aef36f83e8a888061adc8632f4c9f833
Adam Basiak <basiakus@gmail.com>
2018-06-11 18:53:53 +0200

    test

--- script.js
+++ script.js
@@ -505,8 +505,9 @@ window.onload = function() {
 	exercise12CookieShow.addEventListener('click', function() {
 		const cookies = document.cookie.split('; ');
 		for (let i = 0; i < cookie.length; i++) {
-			exercise12CookieList.innerHTML = cookie[i];
+			exercise12CookieList.innerHTML = cookies[i];
 		}
+		return cookies;
 	});
 
 
